7289143cd1c6e5da0051917c7f1c00ce28c60d4c
Maciej Boniewicz <amossoma@o2.pl>
2018-03-19 22:28:54 +0100

    Gradle wrapper, main class

---
+++ src/main/java/org/amossoma/jackson/dataformat/lua/LuaMapper.java
@@ -0,0 +1,16 @@
+package org.amossoma.jackson.dataformat.lua;
+
+import java.util.Collection;
+
+import com.fasterxml.jackson.core.type.TypeReference;
+
+import com.fasterxml.jackson.databind.*;
+import com.fasterxml.jackson.databind.cfg.MapperBuilder;
+import com.fasterxml.jackson.databind.cfg.MapperBuilderState;
+import com.fasterxml.jackson.databind.introspect.AnnotatedMember;
+import com.fasterxml.jackson.databind.introspect.BeanPropertyDefinition;
+import com.fasterxml.jackson.databind.util.NameTransformer;
+import com.fasterxml.jackson.databind.util.SimpleLookupCache;
+
+public class LuaMapper {
+}
