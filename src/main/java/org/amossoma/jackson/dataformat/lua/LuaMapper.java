f0a1221e96970f7eb05357b4b8c7cc2ddc25b27e
Maciej Boniewicz <amossoma@o2.pl>
2018-03-20 22:57:13 +0100

    Class is fixed, build gradle

--- src/main/java/org/amossoma/jackson/dataformat/lua/LuaMapper.java
+++ src/main/java/org/amossoma/jackson/dataformat/lua/LuaMapper.java
@@ -1,16 +1,7 @@
 package org.amossoma.jackson.dataformat.lua;
 
-import java.util.Collection;
+import com.fasterxml.jackson.databind.ObjectMapper;
 
-import com.fasterxml.jackson.core.type.TypeReference;
+public class LuaMapper extends ObjectMapper {
 
-import com.fasterxml.jackson.databind.*;
-import com.fasterxml.jackson.databind.cfg.MapperBuilder;
-import com.fasterxml.jackson.databind.cfg.MapperBuilderState;
-import com.fasterxml.jackson.databind.introspect.AnnotatedMember;
-import com.fasterxml.jackson.databind.introspect.BeanPropertyDefinition;
-import com.fasterxml.jackson.databind.util.NameTransformer;
-import com.fasterxml.jackson.databind.util.SimpleLookupCache;
-
-public class LuaMapper {
 }
